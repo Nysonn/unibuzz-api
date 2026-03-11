6807b7ca0da236f84c35423a46c70bf21d9a06f2
Nysonn <nysonagumya@gmail.com>
2026-03-11 18:26:17 +0300

    OOm bug

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -18,8 +18,8 @@ func NewPostgresPool(databaseURL string) *pgxpool.Pool {
 		log.Fatal("Unable to parse database URL:", err)
 	}
 
-	config.MaxConns = 20
-	config.MinConns = 2
+	config.MaxConns = 10
+	config.MinConns = 1
 
 	pool, err := pgxpool.NewWithConfig(ctx, config)
 	if err != nil {
